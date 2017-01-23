a652a8357907302530c23799b5443379fe83c92b
taohouy <taohouy@gmail.com>
2017-01-23 18:24:24 +0700

    Update verifywebhook.php

--- verifywebhook.php
+++ verifywebhook.php
@@ -1,6 +1,6 @@
 <?php
 $access_token = "EAAMpHeTlEBgBAJRnnmCiRf6OAMtrRNDwTaZB5Pn1Fhz9v4FtGnbXWJp7GZB2j8vl2ZBDsCwdptp4c4BIleFSZCKNLPxlPL5SdouY4ZC5LwCx1mhWN0t3tjidkBU7DVSw3r3ZBZA6fgRIRhAFxZCqRZCg72zs9a4r4t9NAL1UZAjie02wZDZD";
-echo $verify_token = "mjulibrarybot";
+$verify_token = "mjulibrarybot";
 $hub_verify_token = null;
 if(isset($_REQUEST['hub_challenge'])) {
     $challenge = $_REQUEST['hub_challenge'];
